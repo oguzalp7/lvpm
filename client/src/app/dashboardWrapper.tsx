61bb667585f751f405b3a1d4ce6d7ae5943ff5a9
Oguz Alp Saglam <oguzalp7@github.com>
2025-10-21 11:38:09 +0300

    first commit

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const DashboardWrapper = ({children}: Props) => {
+  return (
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+        sidebar
+
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+          navbar
+          {children}    
+        </main>
+    </div>
+  )
+}
+
+export default DashboardWrapper
